411c7f4e14c19a7348fad5ff5e9e349002fe0c96
John Pork <mbebyk@outlook.com>
2026-05-15 18:20:12 +0300

    deploy changes

--- server/database.js
+++ server/database.js
@@ -1,12 +1,10 @@
 require("dotenv").config();
 const mysql = require('mysql2');
 
-const pool = mysql.createPool({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
-}).promise();
+
+const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQL_ROOT_PASSWORD}@${process.env.RAILWAY_PRIVATE_DOMAIN}:3306/${process.env.MYSQL_DATABASE}`;
+
+const pool = mysql.createPool(urlDB).promise();
 
 
 module.exports = pool;
